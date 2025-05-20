19f58b6dc94abedac77e400fab65c27b465be687
Sahebzad Kushal Deep <kushaldeep448@gmail.com>
2025-05-20 12:45:47 +0530

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,37 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [user,Setuser]=useState("")
+  const [pass,setPass]=useState("")
+  const [error,setError]=useState("")
+
+
+const handleSubmit = () => {
+  if(user.trim()==="" || pass.trim()===""){
+    setError("Both the fields are required.")
+
+
+  }
+  else{
+    setError("")
+  }
+}
+
   return (
     <div>
         {/* Do not remove the main div */}
+        
+
+        <input  id="username" type="text" value={user} onChange={(e)=>Setuser(e.target.value)}/>
+        <input  id="password" type="password" value={pass} onChange={(e)=>setPass(e.target.value)}/>
+
+        <button onClick={handleSubmit}>Submit</button>
+
+        {
+          error &&<p id="errorMessage" style={{color:"red"}}>{error}</p>
+        }
     </div>
   )
 }
